d18695deafa042bf180326783a2fd9736fa50a4c
philippe-bn <83093735+philippe-bn@users.noreply.github.com>
2026-01-13 14:59:19 -0500

    Started Task1 and completed Qu 1

---
+++ tasks/task1/js/script.js
@@ -0,0 +1,34 @@
+"use strict";
+
+function setup() {
+    console.log("go")
+    createCanvas(300, 300);
+
+    // QU 2
+
+
+}
+
+function draw() {
+    background(0);
+
+    // // QU 1
+    // noStroke();
+    // push();
+    // fill(128, 0, 128);
+    // ellipse(10, 20, 10);
+    // pop();
+    // push();
+    // fill(255, 0, 255);
+    // ellipse(30, 40, 20);
+    // pop();
+    // push();
+    // fill(75, 0, 130);
+    // ellipse(60, 70, 30);
+    // pop();
+
+}
+
+function drawEllipse(x, y, w, h, r, g, b) {
+
+}
