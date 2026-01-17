f4a3beeacd1d22f2fd4679c0797ae26d8a7d80a3
philippe-bn <83093735+philippe-bn@users.noreply.github.com>
2026-01-17 17:01:51 -0500

    Finished Qu 6 (no bonus)

--- tasks/task1/js/question6.js
+++ tasks/task1/js/question6.js
@@ -1,85 +1,49 @@
 "use strict";
-// QU 4.0
-// fill object
-let colours = {
-    rect1: {
-        r: 0,
-        g: 0,
-        b: 255
-    },
-    rect2: {
-        r: 100,
-        g: 100,
-        b: 255
-    },
-    rect3: {
-        r: 150,
-        g: 150,
-        b: 255
-    }
+
+let textObject = {
+    string: "test",
+    fill: "white",
+    textSize: 28,
+    x: 150,
+    y: 150
 }
+
+const horizontalNumbersAmount = 10;
+const verticalNumbersAmount = 15;
+const numberSpacing = 20;
 
 function setup() {
     console.log("go")
-    createCanvas(300, 300);
+    createCanvas(500, 500);
 }
 
 function draw() {
-    // QU 4.1
     background(0);
-    noStroke();
+
     push();
-    fill(colours.rect1.r, colours.rect1.g, colours.rect1.b);
-    rect(0, 0, width / 3, height);
-    pop();
-    push();
-    fill(colours.rect2.r, colours.rect2.g, colours.rect2.b);
-    rect(100, 0, width / 3, height);
-    pop();
-    push();
-    fill(colours.rect3.r, colours.rect3.g, colours.rect3.b);
-    rect(200, 0, width / 3, height);
+    fill(textObject.fill);
+    textAlign(CENTER, CENTER);
+    textSize(textObject.textSize);
+    text(textObject.string, textObject.x, textObject.y);
     pop();
 
-    checkRect();
-}
+    let initialX = 20;
+    let initialY = 30;
 
-
-// QU 4.2
-function checkRect() {
-    // Overlapping rectangle 1
-    if (mouseX > 0 && mouseX < 100) {
-        colours.rect1.r = 255;
-        colours.rect1.g = 255;
-        colours.rect1.b = 255;
-    }
-    else {
-        colours.rect1.r = 0;
-        colours.rect1.g = 0;
-        colours.rect1.b = 255;
+    // horizontal numbers, start at 0, end at 9
+    for (let i = 0; i < horizontalNumbersAmount; i++) {
+        fill(textObject.fill);
+        textSize(textObject.textSize);
+        text(i, initialX + i * numberSpacing, initialY);
     }
 
-    // Overlapping rectangle 2
-    if (mouseX > 100 && mouseX < 200) {
-        colours.rect2.r = 255;
-        colours.rect2.g = 255;
-        colours.rect2.b = 255;
-    }
-    else {
-        colours.rect2.r = 100;
-        colours.rect2.g = 100;
-        colours.rect2.b = 255;
-    }
+    initialX = 20;
+    initialY = 30;
 
-    // Overlapping rectangle 3
-    if (mouseX > 200 && mouseX < 300) {
-        colours.rect3.r = 255;
-        colours.rect3.g = 255;
-        colours.rect3.b = 255;
-    }
-    else {
-        colours.rect3.r = 150;
-        colours.rect3.g = 150;
-        colours.rect3.b = 255;
+    // vertical numbers, start at 15, end at 1 (ascending)
+    for (let i = verticalNumbersAmount; i > 0; i--) {
+        fill(textObject.fill);
+        textSize(textObject.textSize);
+        text(i, initialX, initialY + i * numberSpacing);
     }
 }
