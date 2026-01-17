14012e0ba63c9143c9cf94eced9584a3b4957cd1
philippe-bn <83093735+philippe-bn@users.noreply.github.com>
2026-01-17 17:41:40 -0500

    Finished Qu 7, finished Task 1

--- tasks/task1/js/question7.js
+++ tasks/task1/js/question7.js
@@ -1,85 +1,57 @@
 "use strict";
-// QU 4.0
-// fill object
-let colours = {
-    rect1: {
-        r: 0,
-        g: 0,
-        b: 255
-    },
-    rect2: {
-        r: 100,
-        g: 100,
-        b: 255
-    },
-    rect3: {
-        r: 150,
-        g: 150,
-        b: 255
+
+let circle = {
+    size: 30, // can become any multiple of 5
+    fill: {
+        r: undefined,
+        g: undefined,
+        b: undefined
     }
 }
+
+let state = "circle";
 
 function setup() {
     console.log("go")
     createCanvas(300, 300);
+
+    assignColour();
 }
 
 function draw() {
-    // QU 4.1
     background(0);
-    noStroke();
-    push();
-    fill(colours.rect1.r, colours.rect1.g, colours.rect1.b);
-    rect(0, 0, width / 3, height);
-    pop();
-    push();
-    fill(colours.rect2.r, colours.rect2.g, colours.rect2.b);
-    rect(100, 0, width / 3, height);
-    pop();
-    push();
-    fill(colours.rect3.r, colours.rect3.g, colours.rect3.b);
-    rect(200, 0, width / 3, height);
-    pop();
 
-    checkRect();
+    for (let x = 0; x < width; x++) {
+        for (let y = 0; y < height; y++) {
+            fill(circle.fill.r, circle.fill.g, circle.fill.b);
+            if (state === "circle") {
+                ellipse((circle.size / 2) + x * circle.size, (circle.size / 2) + y * circle.size, circle.size);
+            }
+            else if (state === "square") {
+                rect(x * circle.size, y * circle.size, circle.size);
+            }
+        }
+
+    }
 }
 
+function assignColour() {
+    circle.fill.r = random(255);
+    circle.fill.g = random(255);
+    circle.fill.b = random(255);
+}
 
-// QU 4.2
-function checkRect() {
-    // Overlapping rectangle 1
-    if (mouseX > 0 && mouseX < 100) {
-        colours.rect1.r = 255;
-        colours.rect1.g = 255;
-        colours.rect1.b = 255;
+function keyPressed() {
+    if (keyCode === 32) {
+        assignColour();
     }
-    else {
-        colours.rect1.r = 0;
-        colours.rect1.g = 0;
-        colours.rect1.b = 255;
-    }
+}
 
-    // Overlapping rectangle 2
-    if (mouseX > 100 && mouseX < 200) {
-        colours.rect2.r = 255;
-        colours.rect2.g = 255;
-        colours.rect2.b = 255;
+function mouseClicked() {
+    if (state === "circle") {
+        state = "square";
     }
-    else {
-        colours.rect2.r = 100;
-        colours.rect2.g = 100;
-        colours.rect2.b = 255;
-    }
-
-    // Overlapping rectangle 3
-    if (mouseX > 200 && mouseX < 300) {
-        colours.rect3.r = 255;
-        colours.rect3.g = 255;
-        colours.rect3.b = 255;
-    }
-    else {
-        colours.rect3.r = 150;
-        colours.rect3.g = 150;
-        colours.rect3.b = 255;
+    else if (state === "square") {
+        state = "circle";
     }
 }
